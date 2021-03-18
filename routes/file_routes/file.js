f29a264714c8acc9d7ac0ab87853c296c0d7ef28
pawan1210 <pawansaggu007@gmail.com>
2021-03-18 19:24:56 +0530

    Fixed download attachments error

--- routes/file_routes/file.js
+++ routes/file_routes/file.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const path=require("path");
 
 router.get("/:file_name/view", (req, res) => {
   const filePath = path.join(__dirname, "uploads", req.params.file_name);
